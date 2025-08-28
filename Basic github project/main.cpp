939fd4d7f45daf5ba466e70bea55ee4fa46cd861
kendallr-w <reedwarrenkj@gmail.com>
2025-08-28 10:18:21 -0500

    Update main.cpp

--- Basic github project/main.cpp
+++ Basic github project/main.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 int main()
 	{
-		std:cout < "asdaf\n";  //version control
+		std::cout << "asdaf. \n";  //version control
 
-		std::cout << "asdaeeeef\n"  //version control
+		std::cout << "asdaeeeef. \n";  //version control
 
-		//what does \n do? it moves the cursor to the nect line
+		//what does \n do? it moves the cursor to the next line
 	}
